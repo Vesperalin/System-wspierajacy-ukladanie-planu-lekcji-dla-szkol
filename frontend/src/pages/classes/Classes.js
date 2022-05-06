31a62c6296f2fdff1543e97c4cee54c878bec1fb
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2022-05-06 20:40:40 +0200

    Add table to subject

--- frontend/src/pages/classes/Classes.js
+++ frontend/src/pages/classes/Classes.js
@@ -1,5 +1,54 @@
+import { useState } from 'react';
+
+import ElementsTable from '../../components/elements-table/ElementsTable';
+import style from './Classes.module.scss';
+import Button from '../../components/button/Button';
+
+const mockedClasses = [
+	{ ID_Class: 'IIa', Year: 2021 },
+	{ ID_Class: 'Ib', Year: 2022 },
+	{ ID_Class: 'IIIb', Year: 2019 },
+	{ ID_Class: 'IIc', Year: 2021 },
+];
+
 const Classes = () => {
-	return <>Classes</>;
+	const [classes, setClasses] = useState(mockedClasses);
+
+	const onDelete = school_class => {
+		// tu będzie obsługa dla delete
+		console.log(school_class);
+	};
+
+	const onEdit = school_class => {
+		// tu będzie obsługa dla edit
+		console.log(school_class);
+	};
+
+	const onAdd = () => {
+		// tu będzie obsługa dla add
+		console.log('add');
+	};
+
+	if (classes.length > 0) {
+		const elementHeaders = ['Class', 'Year'];
+		return (
+			<div className={style['table-wrapper']}>
+				<ElementsTable
+					elements={classes}
+					title='Subjects'
+					headers={elementHeaders}
+					actions={[
+						['Delete', onDelete],
+						['Edit', onEdit],
+					]}
+				/>
+				<Button onClick={onAdd} text='Add' />
+			</div>
+		);
+	} else {
+		// loading bar
+		return <>Classes</>;
+	}
 };
 
 export default Classes;
