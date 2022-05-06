a493f9d9b8065c6c51b5ef0898b49f9d908e9f2f
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2022-05-06 16:47:08 +0200

    Implement main page and navbar

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -1,5 +1,18 @@
+import style from './Home.module.scss';
+import schoolImage from '../../assets/school.jpg';
+
 const Home = () => {
-	return <>Home</>;
+	return (
+		<div className={style['home-wrapper']}>
+			<img src={schoolImage} alt='school classroom' />
+			<div className={style['panel-wrapper']}>
+				<div className={style.panel}>
+					<h2>Create lessons plan </h2>
+					<h2>for your school</h2>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
