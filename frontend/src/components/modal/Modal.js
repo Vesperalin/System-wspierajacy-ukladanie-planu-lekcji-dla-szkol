49ec06e7f137b9d3cbe2e7e9082d77e8e8e6d5c0
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2022-05-08 15:33:48 +0200

    Add modal

---
+++ frontend/src/components/modal/Modal.js
@@ -0,0 +1,53 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+
+import Button from '../button/Button';
+import style from './Modal.module.scss';
+
+const Backdrop = props => {
+	return <div className={style.backdrop} onClick={props.onClick}></div>;
+};
+
+const ModalOverlay = props => {
+	return (
+		<div className={style.modal}>
+			<header className={style.header}>
+				<h2>{props.title}</h2>
+			</header>
+			{props.children}
+			<footer className={style.actions}>
+				<button onClick={props.onReject} className={style['secondary-button']}>
+					{props.onRejectText}
+				</button>
+				<button className={style['primary-button']} onClick={props.onAccept}>
+					{props.onAcceptText}
+				</button>
+			</footer>
+		</div>
+	);
+};
+
+const Modal = props => {
+	return (
+		<Fragment>
+			{ReactDOM.createPortal(
+				<Backdrop onClick={props.onClick} />,
+				document.getElementById('backdrop-root'),
+			)}
+			{ReactDOM.createPortal(
+				<ModalOverlay
+					title={props.title}
+					onAcceptText={props.onAcceptText}
+					onRejectText={props.onRejectText}
+					onAccept={props.onAccept}
+					onReject={props.onReject}
+				>
+					{props.children}
+				</ModalOverlay>,
+				document.getElementById('overlay-root'),
+			)}
+		</Fragment>
+	);
+};
+
+export default Modal;
