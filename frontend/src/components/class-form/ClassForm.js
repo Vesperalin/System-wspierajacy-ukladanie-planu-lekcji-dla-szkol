4e83fe042bbe61e4eac4fe5c43d2017d610b135e
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2022-05-08 12:03:56 +0200

    Add form for classes

---
+++ frontend/src/components/class-form/ClassForm.js
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import style from './ClassForm.module.scss';
+
+const isPositive = value => value >= 0;
+const isNotEmpty = value => value.trim() !== '';
+
+const ClassForm = props => {
+	const navigate = useNavigate();
+	const [isClassValid, setIsClassValid] = useState(true);
+	const [isYearValid, setIsYearValid] = useState(true);
+
+	const onSubmitHandler = event => {
+		event.preventDefault();
+		const ifInputValid = isClassValid && isYearValid;
+
+		if (!ifInputValid) {
+			props.setErrorMessage(
+				'The input is invalid. Year must be positive and class name not empty!',
+			);
+			return;
+		} else {
+			props.setErrorMessage('');
+			props.onSubmit();
+			navigate('/classes');
+		}
+	};
+
+	const onClassHandler = event => {
+		const className = event.target.value;
+
+		if (isNotEmpty(className)) {
+			setIsClassValid(true);
+		} else {
+			setIsClassValid(false);
+		}
+
+		props.setClassName(className);
+	};
+
+	const onYearHandler = event => {
+		const year = event.target.value;
+
+		if (isPositive(year)) {
+			setIsYearValid(true);
+		} else {
+			setIsYearValid(false);
+		}
+
+		props.setYear(year);
+	};
+
+	const classStyleClasses = `${isClassValid ? '' : style.invalid}`;
+	const yearStyleClasses = `${isYearValid ? '' : style.invalid}`;
+
+	return (
+		<div className={style.content} onSubmit={onSubmitHandler}>
+			<h1>{props.formTitle}</h1>
+			<form>
+				<label htmlFor='class-name'>Class name</label>
+				<input
+					id='class-name'
+					type='text'
+					value={props.className}
+					onChange={onClassHandler}
+					className={classStyleClasses}
+					required
+				/>
+				<label htmlFor='year'>Year</label>
+				<input
+					id='year'
+					type='number'
+					value={props.year}
+					onChange={onYearHandler}
+					className={yearStyleClasses}
+					required
+					min='1900'
+					max='3099'
+					step='1'
+				/>
+				<div className={style['action-button-wrapper']}>
+					<button className={style.button}>{props.actionText}</button>
+				</div>
+			</form>
+			{props.errorMessage !== '' && (
+				<p className={style.error}>{props.errorMessage}</p>
+			)}
+			<button onClick={() => navigate('/classes')} className={style.button}>
+				&larr; Back
+			</button>
+		</div>
+	);
+};
+
+export default ClassForm;
--- frontend/src/pages/classes/add-class/AddClass.js
+++ frontend/src/pages/classes/add-class/AddClass.js
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+
+import ClassForm from '../../../components/class-form/ClassForm';
+
 const AddClass = () => {
-	return <p>Add class</p>;
+	const [className, setClassName] = useState('');
+	const [year, setYear] = useState(2022);
+	const [errorMessage, setErrorMessage] = useState(''); // tu będzie info o niepowodzeniach - też z backendu
+
+	const onSubmit = () => {
+		console.log('submit formularza dla klas');
+	};
+
+	return (
+		<ClassForm
+			onSubmit={onSubmit}
+			formTitle='Add class'
+			className={className}
+			setClassName={setClassName}
+			year={year}
+			setYear={setYear}
+			actionText='Add'
+			errorMessage={errorMessage}
+			setErrorMessage={setErrorMessage}
+		/>
+	);
 };
 
 export default AddClass;
