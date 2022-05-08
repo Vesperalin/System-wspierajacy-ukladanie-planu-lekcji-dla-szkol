fc0f676bafe87f908cb7bee86c70ddf99eb29e6d
Klaudia Blazyczek <blazyczek.klaudia@gmail.com>
2022-05-08 14:53:04 +0200

    Add login panel

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -1,5 +1,29 @@
+import googleLogo from '../../assets/google_logo.png';
+
+import style from './Login.module.scss';
+
 const Login = () => {
-	return <>Login</>;
+	return (
+		<div className={style.wrapper}>
+			<h1>Login</h1>
+			<div className={style.panel}>
+				<div className={style.action}>
+					<h3>Log in to app:</h3>
+					<button>
+						<img className={style.logo} src={googleLogo} alt='google' />
+						<p>Sign In with Google</p>
+					</button>
+				</div>
+				<div className={style.action}>
+					<h3>Don't have an account? Sign up!</h3>
+					<button>
+						<img className={style.logo} src={googleLogo} alt='google' />
+						<p>Sign Up with Google</p>
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Login;
